77de6ffb7056b459af934ad8f4259849f8edb0ea
KmanT <kturske@gmail.com>
2019-04-07 13:45:24 -0400

    Started Cloud Functions
    Added the cloud functions folder. Half of the project should be ready by this point

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { signIn } from '../../store/actions/authActions';
+import { Redirect } from 'react-router-dom';
 
 class SignIn extends Component {
     state = {
@@ -20,6 +21,9 @@ class SignIn extends Component {
     }
 
     render() {
+        const { authError, auth } = this.props;
+        if (auth.uid) return <Redirect to='/' />;
+
         return <div className="container">
             <form onSubmit={this.handleSubmit} className="white">
                 <h5 className="grey-text text-darken-3">Sign In</h5>
@@ -33,6 +37,9 @@ class SignIn extends Component {
                 </div>
                 <div className="input-field">
                     <button className="btn pink lighten-1 z-depth-0">Login</button>
+                    <div className="red-text center">
+                        { authError ? <p>{authError}</p> : null}
+                    </div>
                 </div>
             </form>
          </div>
@@ -41,7 +48,8 @@ class SignIn extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        authErr: state.auth.authErr
+        authError: state.auth.authError,
+        auth: state.firebase.auth
     }
 }
 
@@ -51,7 +59,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(SignIn);
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,19 +5,21 @@ import SignedOutLinks from './SignedOutLinks';
 import { connect } from 'react-redux';
 
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth, profile } = props;
+    const links = auth.uid ? <SignedInLinks profile={profile} /> : <SignedOutLinks/>;
     return <nav className="nav-wrapper grey darken-3">
         <div className="container">
             <Link to="/" className="brand-logo left">Project Planner</Link>
-            <SignedInLinks />
-            <SignedOutLinks />
+            { links }
         </div>
     </nav>
 }
 
 const mapStateToProps = (state) => {
     return {
-
+        auth: state.firebase.auth,
+        profile: state.firebase.profile
     }
 }
 
